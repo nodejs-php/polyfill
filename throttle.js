ed43660e0b4db7497ea88ed45087a72215022cd7
aleksey <av.pavlow@gmail.com>
2023-12-13 10:19:40 +0300

    Добавляет комментарии по дросселированию

--- throttle.js
+++ throttle.js
@@ -1,16 +1,35 @@
+/**
+ * Надо ли дросселировать
+ * @type {boolean}
+ */
+let shouldThrottle = false;
+
+/**
+ *
+ * Дросселирование функции, запуск некоторого другого обработчика с некоторым интервалом времени
+ *
+ * @param func
+ * @param wait
+ * @returns {(function(...[*]): void)|*}
+ */
 export default function throttle(func, wait) {
-    let shouldThrottle = false;
 
-  return function (...args) {
-    if (shouldThrottle) {
-      return;
-    }
+    return function (...args) {
+        //Не запускаем новую обработку пока таймер не завершил свою работу
+        if (shouldThrottle) {
+            return;
+        }
 
-    shouldThrottle = true;
-    setTimeout(function () {
-      shouldThrottle = false;
-    }, wait);
+        //запрещаем запуск дросселирования
+        shouldThrottle = true;
 
-    func.apply(this, args);
-  };
+        //Запускает в асинхронном режиме таймер
+        setTimeout(function () {
+            //разрешаем запуск дросселирования
+            shouldThrottle = false;
+        }, wait);
+
+        //Запускает исходную функцию
+        func.apply(this, args);
+    };
 }
