902dc01cb4776245348a1650677325450c53db76
aleksey <av.pavlow@gmail.com>
2023-12-16 22:25:40 +0300

    Добавляет комментарии к функциям полифилла операций массива

--- array-methods-polyfill.js
+++ array-methods-polyfill.js
@@ -1,71 +1,121 @@
+/**
+ * Прототип функции для массива Array для обхода всех элементов и выполнения над ними операции `callback`
+ * @param callback Выполняемая функция
+ * @returns {[]} Возвращается как результат массив
+ */
 Array.prototype.myMap = function (callback) {
     const output = [];
-    for(let i = 0; i < this.length; i++) {
-      output.push(callback(this[i],i,this));
+    //Цикл по всем элементам
+    for (let i = 0; i < this.length; i++) {
+        //Выполняем функцию `callback` и кладем результат в массив `output`
+        output.push(callback(this[i], i, this));
     }
-    return output;
-  };
 
-  Array.prototype.myMap = function (callbackFn, thisArg) {
+    return output;
+};
+
+/**
+ * Прототипа функции для обхода всех элементов массива `thisArg` и выполнения над ними  операции  `callback`
+ * @param callbackFn Выполняемая функция
+ * @param thisArg массив
+ * @returns {[]} Возвращается как результат массив
+ */
+Array.prototype.myMap = function (callbackFn, thisArg) {
     const output = [];
     for (let i = 0; i < this.length; i++) {
-      if (Object.hasOwn(this, i)) {
-        output[i] = callbackFn.call(thisArg, this[i], i, this);
-      }
-    }
-    return output;
-  };
-  
-  Array.prototype.myFilter = function (callback) {
-    const output = [];
-    for(let i = 0; i < this.length; i++) {
-      if(callback(this[i],i,this) === true) {
-        output.push(this[i]);
-      }
-    }
-    return output;
-  };
-
-  Array.prototype.myFilter = function (callbackFn, thisArg) {
-    const output = [];
-      for(let i = 0; i < this.length; i++) {
-        if(Object.hasOwn(this,i) && callbackFn.call(thisArg,this[i],i,this) === true) {
-          output.push(this[i]);
+        if (Object.hasOwn(this, i)) {
+            output[i] = callbackFn.call(thisArg, this[i], i, this);
         }
-      }
-      return output;
-  };
-
-  
-  Array.prototype.myReduce = function (callback, initialValue) {
-  let accumulator = initialValue;
-    for(let i = 0; i <this.length; i++) {
-      if(i === 0 && initialValue === undefined) {
-        accumulator = this[i];
-      } else {
-            accumulator = callback(accumulator, this[i], i, this);
-      }
-  
     }
-    return accumulator;
-  };
 
-  Array.prototype.myReduce = function (callbackFn, initialValue) {
+    return output;
+};
+
+/**
+ * Фильтрация массива
+ * @param callback Функция фильтрации  возвращающая true/false, 1/0
+ * @returns {[]}
+ */
+Array.prototype.myFilter = function (callback) {
+    const output = [];
+    for (let i = 0; i < this.length; i++) {
+        //Проверка условия
+        if (callback(this[i], i, this) === true) {
+            output.push(this[i]);
+        }
+    }
+
+    return output;
+};
+
+/**
+ * Фильтрация массива
+ * @param callbackFn
+ * @param thisArg Массив
+ * @returns {[]}
+ */
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+    const output = [];
+    for (let i = 0; i < this.length; i++) {
+        //Проверка существования индекса элемента в массиве, и выполнения условия проверки заданной функцией `callbackFn`
+        if (Object.hasOwn(this, i) && callbackFn.call(thisArg, this[i], i, this) === true) {
+            output.push(this[i]);
+        }
+    }
+
+    return output;
+};
+
+/**
+ * Получение результирующего значения, накапливаемой функцией `callback`
+ * @param callback Функция
+ * @param initialValue Начальное значение
+ * @returns {*}
+ */
+Array.prototype.myReduce = function (callback, initialValue) {
+    //Инициализация накопителя начальным значением
     let accumulator = initialValue;
-      if (initialValue === undefined && this.length === 0) {
-      throw new TypeError('Reduce of empty array with no initial value');
-    }
-      for(let i = 0; i <this.length; i++) {
-        if(i === 0 && initialValue === undefined) {
-          accumulator = this[i];
+
+    for (let i = 0; i < this.length; i++) {
+        if (i === 0 && initialValue === undefined) {
+            //Если начальное значение не задано, то инициализируем накопитель начальным значением массива
+            accumulator = this[i];
         } else {
-          if(Object.hasOwn(this, i)) {
-   accumulator = callbackFn(accumulator, this[i], i, this);
-          }
-             
+            //Вычисляем значение `callback`-функции по отношению к элементу
+            accumulator = callback(accumulator, this[i], i, this);
         }
-    
-      }
-      return accumulator;
-  };
+    }
+
+    return accumulator;
+};
+
+/**
+ * Получение результирующего значения функции, накапливаемой функцией `callbackFn`
+ * @param callbackFn Функция
+ * @param initialValue Инициализируемое значение
+ *
+ * @returns {*} Возвращаемое значение
+ */
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+    //Устанавливаем начальное значение которое может быть любого типа
+    let accumulator = initialValue;
+    //Если массив пустой или неопределенный, выбрасываем ошибку
+    if (initialValue === undefined && this.length === 0) {
+        throw new TypeError('Reduce of empty array with no initial value');
+    }
+    for (let i = 0; i < this.length; i++) {
+        //Если не задано начальное значение, то инициализируем накопитель 1-м элементом массива
+        if (i === 0 && initialValue === undefined) {
+            accumulator = this[i];
+        } else {
+            //Если существует такой индекс массива
+            if (Object.hasOwn(this, i)) {
+                //Вычисляем значение и присваиваем результат
+                accumulator = callbackFn(accumulator, this[i], i, this);
+            }
+        }
+    }
+
+    return accumulator;
+};
   
--- customPromise.sol.js
+++ customPromise.sol.js
@@ -1,102 +1,165 @@
-/* Polyfill of promise */
-
+/**
+ * Статусы промисов
+ *
+ * @type {{FULFILLED: string, PENDING: string, REJECTED: string}}
+ */
 const STATE = {
     PENDING: 'pending',
     FULFILLED: 'fulfilled',
     REJECTED: 'rejected'
-  };
-  class MyPromise {
+};
+
+/**
+ * Реализация промисов
+ * Полифил для промисов
+ */
+class MyPromise {
+    /**
+     * Устанавливаем начальный статус
+     * @type {string}
+     */
     #state = STATE.PENDING;
     #value = null;
+
+    /**
+     * Выполненные коллбэки
+     * @type {[]}
+     */
     #fulfilledCallbacks = [];
+
+    /**
+     * Откаченные коллбэки
+     *
+     * @type {[]}
+     */
     #rejectedCallbacks = [];
-  
+
+    /**
+     * Конструктор класса
+     *
+     * @param executorFunc Функция которая оборачивается в промис и выполняется. Это тот функционал который мы хотим выполнить
+     * формат входного параметра-функции (res, rej) => {res(10)}
+     */
     constructor(executorFunc) {
-      try {
-        executorFunc(value => this.#resolve(value), value => this.#reject(value))
-      }catch(error) {
-        this.#reject(error)
-      }
-    }
-  
-    then(onFulfilled, onRejected) {
-      return new MyPromise((resolve, reject) => {
-        const fulfilledCallback = () => {
-          if(!onFulfilled) {
-            return resolve(this.#value);
-          }
-          queueMicrotask(() => {
-            try {
-              const value = onFulfilled(this.#value);
-              resolve(value);           
-            }catch(error) {
-              reject(error)
-            }
-          })
-        };
-        const rejectCallback = () => {
-                  if(!onRejected) {
-            return reject(this.#value);
-          }
-          queueMicrotask(() => {
-            try {
-              const value = onRejected(this.#value);
-              resolve(value);           
-            }catch(error) {
-              reject(error)
-            }
-          })
+        try {
+            executorFunc(value => this.#resolve(value), value => this.#reject(value))
+        } catch (error) {
+            //При неудаче вызываем this.#reject
+            this.#reject(error)
         }
-              switch(this.#state) {
-          case STATE.PENDING:
-            this.#fulfilledCallbacks.push(fulfilledCallback);
-            this.#rejectedCallbacks.push(rejectCallback);
-            break;
-          case STATE.FULFILLED:
-                fulfilledCallback();
-                break;
-          case STATE.REJECTED:
-                rejectCallback();
-                break;
-          default:
-            throw new Error('Unexpected promise state')
-        } 
-      });   
     }
-  
+
+    /**
+     * Реализация then
+     *
+     * @param onFulfilled
+     * @param onRejected
+     * @returns {MyPromise} Возвращаем сам промис как результат для продолжения цепочки вызовов `then()`
+     */
+    then(onFulfilled, onRejected): MyPromise {
+        //Создаем объект самого промиса
+        return new MyPromise((resolve, reject) => {
+            //Создаем функцию-коллбэк
+            const fulfilledCallback = () => {
+                if (!onFulfilled) {
+                    return resolve(this.#value);
+                }
+                //Добавляем в микрозадачи чтобы выполнить коллбэк, который обернут в промис
+                queueMicrotask(() => {
+                    try {
+                        const value = onFulfilled(this.#value);
+                        resolve(value);
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            };
+            const rejectCallback = () => {
+                if (!onRejected) {
+                    return reject(this.#value);
+                }
+                queueMicrotask(() => {
+                    try {
+                        const value = onRejected(this.#value);
+                        resolve(value);
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            }
+            switch (this.#state) {
+                case STATE.PENDING:
+                    //Добавляем в массив коллбэков для выполнения
+                    this.#fulfilledCallbacks.push(fulfilledCallback);
+                    this.#rejectedCallbacks.push(rejectCallback);
+                    break;
+                case STATE.FULFILLED:
+                    fulfilledCallback();
+                    break;
+                case STATE.REJECTED:
+                    rejectCallback();
+                    break;
+                default:
+                    throw new Error('Unexpected promise state')
+            }
+        });
+    }
+
     catch(onRejected) {
-      return this.then(null, onRejected)
+        return this.then(null, onRejected)
     }
-  
+
+    /**
+     * Для получения извне статуса промиса
+     *
+     * @returns {string}
+     */
     get state() {
-      return this.#state;
+        return this.#state;
     }
-  
+
+    /**
+     * Для получения извне результат выполнения промиса, передаваемый в then
+     * @returns {null}
+     */
     get value() {
-      return this.#value;
+        return this.#value;
     }
+
+    /**
+     * Приватная функция выполнения коллбэка
+     *
+     * @param value
+     */
     #resolve(value) {
-      this.#value = value;
-      this.#state = STATE.FULFILLED;
-      this.#fulfilledCallbacks.forEach(callback => callback());
+        this.#value = value;
+        this.#state = STATE.FULFILLED;
+        this.#fulfilledCallbacks.forEach(callback => callback());
     }
-  
+
+    /**
+     * Приватная функция отката коллбэка
+     *
+     * @param value
+     */
     #reject(value) {
-      this.#value = value;
-      this.#state = STATE.REJECTED;
-      this.#rejectedCallbacks.forEach(callback => callback());
+        this.#value = value;
+        this.#state = STATE.REJECTED;
+        this.#rejectedCallbacks.forEach(callback => callback());
     }
-  }
-  
-  new MyPromise((res,rej) => {
+}
+
+
+//Создаем промис и вызываем `then()` для обработки результатов запроса
+new MyPromise((res, rej) => {
     res(10);
-  }).then(value => {
+}).then(value => {
     console.log(value * 2);
     return value * 2;
-  }).then(value => {
+}).then(value => {
     console.log(value * 3);
     return value * 3;
-  })
-  
-  exports.MyPromise = MyPromise;
+})
+
+exports.MyPromise = MyPromise;
   
